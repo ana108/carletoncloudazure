12b822ba38f98692862de54001ea765b3d70e199
ana108 <flutegurl108@hotmail.com>
2016-08-17 07:19:30 -0400

    azure cleanup

--- cloudDev/routes/index.js
+++ cloudDev/routes/index.js
@@ -22,91 +22,20 @@ function index(req, res) {
 	});
 	systemStats.date = new Date();
 	systemStats.listFiles = mySystem.readContentsHome();
-	//sendPage();	
-	//mySystem.proc();
-	//mySystem.processRead();
 	var stuff = "wmic process list";
 	var processes = function(){mySystem.executeCmd(stuff,"\n", function(thisData){
 								if(thisData.indexOf('ERR') > 0){
 									console.log(thisData);
 								}
 								httpd.p = thisData;
-								getHTTPD();// had to link it because otherwise the two calls interfere with each other.
+								environment();// had to link it because otherwise the two calls interfere with each other.
 	});
 							  }
-	processes();
-	//systemStats.processInfo = mySystem.processList;
-	var getHTTPD = function(){
-		var otherStuff = "httpd -V"; //clear
-		mySystem.executeCmd(otherStuff,"\n", function(myData){
-								     systemStats.httpd = myData;
-									 systemStats.confData = mySystem.readDomain(getLocationConfFile());
-									 whoIsLoggedIn();
-												});
-	}
-	var whoIsLoggedIn = function(){
-		var command = "who -H";//"last";
-		mySystem.executeCmd(command,"\n", function(myData){
-								     systemStats.openFile = myData;
-									 whoWasLoggedIn();
-												});
-								  }
-	var whoWasLoggedIn = function(){
-		var command = "last -d"; //returns last users including remote users
-		mySystem.executeCmd(command,"\n", function(myData){
-								     systemStats.remoteLogin = myData;
-									 getUmask();
-												});
-								  }	
-	var getUmask = function(){
-		var otherStuff = "umask";
-		mySystem.executeCmd(otherStuff,"\n",function(myData){
-								     systemStats.umask = myData;
-									 selinux();
-												});			
-							 }
-	function getLocationConfFile(){
-		var path = "";
-		var flag = 0;
-		for(var i = 0; i < systemStats.httpd.length; i++){
-			if(systemStats.httpd[i].indexOf('HTTPD_ROOT') > 0){
-				var result = systemStats.httpd[i].split('=')[1].replace(/"/g, '');
-				flag = flag+1;
-			}
-			if(systemStats.httpd[i].indexOf('SERVER_CONFIG_FILE') > 0){
-				var resultLoc = systemStats.httpd[i].split('=')[1].replace(/"/g, '');
-				flag = flag+1;
-			}
-			if(flag == 2){
-				path = result + '/' + resultLoc;
-				break;
-			}
-		}
-		return path;
-	}
-	var selinux = function(){
-		var command = "sestatus"; 
-		mySystem.executeCmd(command,"\n",function(myData){
-								     systemStats.sestatus = myData;
-									 environment();
-												});	
-	}
+
 	var environment = function(){
 		var command = "env"; 
 		mySystem.executeCmd(command,"\n",function(myData){
 								     systemStats.env = myData;
-									 lastlog();
-												});	
-	}
-	
-	var lastlog = function(){
-		var command = "lastlog"; 
-		mySystem.executeCmd(command,"\n",function(myData){
-								     systemStats.lastlog = myData;
-									 if(myData.length == 0){
-										systemStats.lastlog = [];
-										systemStats.lastlog.push("Permission denied");
-									 }
 									 listPorts();
 												});	
 	}
@@ -139,6 +68,7 @@ function index(req, res) {
 			sendPage();
 		})
 	}
+	
 	function sendPage(){
 		pageComplete += 1;
 		if(pageComplete == 3){
@@ -162,41 +92,9 @@ function users(req, res) {
 	res.send(sending);
     
 };
-
-function processes(req, res) {
-	var fullProcessInfo = "<ol><li>First row</li>";
-	if(info.numProcess.num == null){
-		info.getProcessByUser();
-	}
-	for(var i =0; i < info.processes.length; i++){
-		fullProcessInfo += "<li>"+info.processes[i]+"</li>";
-	}
-	fullProcessInfo += "</ol>";
-    res.send(fullProcessInfo);
-}
-var files = {};
-files.list = [];
-function unownedFiles(req, res){
-	var command = "find / -nogroup -nouser";
-	console.log("Calling find functions...");
-	if(mySystem.data.length == 0){
-	mySystem.executeCmd(command,"\n",function(myData){
-									 var temp = [];
-									 if(myData.length == 0){
-										temp.push("There are no files that do not have an owner.");
-										files.list = temp;
-									 }else{
-										files.list = myData;
-									 }
-									 console.log("files list retrieved: ");
-												});
-								 }
 	res.render('unownedFiles', { title: 'Security on the Cloud', info: files});
 }
 
 router.get('/', index);
 router.get('/users', users);
-router.get('/processByUser', processes);
-router.get('/files', unownedFiles);
-
 module.exports = router;
